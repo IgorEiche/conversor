b731e453898a3abe44c770080dac1321cc7f8a52
Igor Carvalho <igorribcarvalho@gmail.com>
2023-02-18 01:47:23 -0300

    exercício realizado

---
+++ conversor.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main () {
+
+
+	const ebulicaoAguaEmCelsius = 100.0
+	const ebulicaoAguaDeKelvinParaCelcius = ebulicaoAguaEmCelsius + 273.0
+
+
+	fmt.Printf("O ponto de ebulição da água de Kelvin é de %gºCelcius.", ebulicaoAguaDeKelvinParaCelcius)
+
+
+
+	
+
+
+
+}
